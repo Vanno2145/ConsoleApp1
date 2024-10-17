a2e8ef5e6f422446d95deded4ae090109add3b5e
ivanm <ivanm@DESKTOP-F33GBDN>
2024-10-17 18:54:34 +0300

    a

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,196 +2,279 @@
 
 namespace ConsoleAPP
 {
+    class City
+    {
+        private string city_name;
+        private string country_name;
+        private int person_count;
+        private int phone_nums;
+        private string[] regions;
+
+        public City()
+        {
+            city_name = string.Empty;
+            country_name = string.Empty;
+            person_count = 0;
+            phone_nums = 0;
+            regions = null;
+        }
+
+        public City(string city_name, string country_name, int person_count, int phone_nums, string[] regions)
+        {
+            this.city_name = city_name;
+            this.country_name = country_name;
+            this.person_count = person_count;
+            this.phone_nums = phone_nums;
+            this.regions = regions;
+        }
+
+        public void InputCityName(string city_name) {
+            this.city_name = city_name;
+        }
+
+        public void InputCountryName(string country_name) {
+            this.country_name = country_name;    
+        }
+
+        public void InputPersonCount(int person_count) { 
+            this.person_count = person_count;
+        }
+
+        public void InputPhoneNums(int phone_nums) { 
+            this.phone_nums = phone_nums;
+        }
+
+        public void InputRegions(string[] regions) { 
+            this.regions = new string[regions.Length];
+
+            for (int i = 0; i < regions.Length; i++) {
+                this.regions[i] = regions[i];
+            }
+        }
+
+        public string GetCityName()
+        {
+            return this.city_name;
+        }
+        public string GetCountryName()
+        {
+            return this.country_name;
+        }
+        public int GetPersonCount()
+        {
+            return (int) this.person_count;
+        }
+        public int GetPhoneNums()
+        {
+            return (int) this.phone_nums;
+        }
+        public string[] GetRegions()
+        {
+            return this.regions;
+        }
+
+        public void Print() {
+            Console.WriteLine(city_name);
+            Console.WriteLine(country_name);
+            Console.WriteLine(person_count);
+            Console.WriteLine(phone_nums);
+            Console.WriteLine(regions);
+        }
+
+
+
+    }
+
+    class Employee
+    {
+        
+        private string fullName;
+        private DateTime birthDate;
+        private string phone;
+        private string workEmail;
+        private string position;
+        private string jobDescription;
+
+        
+        public Employee()
+        {
+            fullName = string.Empty;
+            birthDate = DateTime.MinValue;
+            phone = string.Empty;
+            workEmail = string.Empty;
+            position = string.Empty;
+            jobDescription = string.Empty;
+        }
+
+        
+        public Employee(string fullName, DateTime birthDate, string phone, string workEmail, string position, string jobDescription)
+        {
+            this.fullName = fullName;
+            this.birthDate = birthDate;
+            this.phone = phone;
+            this.workEmail = workEmail;
+            this.position = position;
+            this.jobDescription = jobDescription;
+        }
+
+        
+        public void InputFullName(string fullName)
+        {
+            this.fullName = fullName;
+        }
+
+        public void InputBirthDate(DateTime birthDate)
+        {
+            this.birthDate = birthDate;
+        }
+
+        public void InputPhone(string phone)
+        {
+            this.phone = phone;
+        }
+
+        public void InputWorkEmail(string workEmail)
+        {
+            this.workEmail = workEmail;
+        }
+
+        public void InputPosition(string position)
+        {
+            this.position = position;
+        }
+
+        public void InputJobDescription(string jobDescription)
+        {
+            this.jobDescription = jobDescription;
+        }
+
+        
+        public string GetFullName()
+        {
+            return this.fullName;
+        }
+
+        public DateTime GetBirthDate()
+        {
+            return this.birthDate;
+        }
+
+        public string GetPhone()
+        {
+            return this.phone;
+        }
+
+        public string GetWorkEmail()
+        {
+            return this.workEmail;
+        }
+
+        public string GetPosition()
+        {
+            return this.position;
+        }
+
+        public string GetJobDescription()
+        {
+            return this.jobDescription;
+        }
+
+        
+        public void Print()
+        {
+            Console.WriteLine("ФИО: " + fullName);
+            Console.WriteLine("Дата рождения: " + birthDate.ToShortDateString());
+            Console.WriteLine("Телефон: " + phone);
+            Console.WriteLine("Рабочий email: " + workEmail);
+            Console.WriteLine("Должность: " + position);
+            Console.WriteLine("Описание служебных обязанностей: " + jobDescription);
+        }
+    }
+
+    class Airplane
+    {
+        
+        private string airplaneName;
+        private string manufacturer;
+        private int yearOfManufacture;
+        private string airplaneType;
+
+        
+        public Airplane()
+        {
+            airplaneName = string.Empty;
+            manufacturer = string.Empty;
+            yearOfManufacture = 0;
+            airplaneType = string.Empty;
+        }
+
+        
+        public Airplane(string airplaneName, string manufacturer, int yearOfManufacture, string airplaneType)
+        {
+            this.airplaneName = airplaneName;
+            this.manufacturer = manufacturer;
+            this.yearOfManufacture = yearOfManufacture;
+            this.airplaneType = airplaneType;
+        }
+
+        
+        public void InputData(string airplaneName)
+        {
+            this.airplaneName = airplaneName;
+        }
+
+        
+        public void InputData(string airplaneName, string manufacturer)
+        {
+            this.airplaneName = airplaneName;
+            this.manufacturer = manufacturer;
+        }
+
+        /
+        public void InputData(string airplaneName, string manufacturer, int yearOfManufacture, string airplaneType)
+        {
+            this.airplaneName = airplaneName;
+            this.manufacturer = manufacturer;
+            this.yearOfManufacture = yearOfManufacture;
+            this.airplaneType = airplaneType;
+        }
+
+        
+        public string GetAirplaneName()
+        {
+            return this.airplaneName;
+        }
+
+        public string GetManufacturer()
+        {
+            return this.manufacturer;
+        }
+
+        public int GetYearOfManufacture()
+        {
+            return this.yearOfManufacture;
+        }
+
+        public string GetAirplaneType()
+        {
+            return this.airplaneType;
+        }
+
+
+        public void Print()
+        {
+            Console.WriteLine("Название самолёта: " + airplaneName);
+            Console.WriteLine("Производитель: " + manufacturer);
+            Console.WriteLine("Год выпуска: " + yearOfManufacture);
+            Console.WriteLine("Тип самолёта: " + airplaneType);
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            //Task 1
-
-            //    int number = int.Parse(Console.ReadLine());
-
-            //    if (number < 1 || number > 100)
-            //    {
-            //        Console.WriteLine("Число вне диапазона!");
-            //    }
-            //    else if (number % 3 == 0 && number % 5 == 0)
-            //    {
-            //        Console.WriteLine("Fizz Buzz");
-            //    }
-            //    else if (number % 3 == 0)
-            //    {
-            //        Console.WriteLine("Fizz");
-            //    }
-            //    else if (number % 5 == 0)
-            //    {
-            //        Console.WriteLine("Buzz");
-            //    }
-            //    else
-            //    {
-            //        Console.WriteLine(number);
-            //    }
-
-            //Task 2
-
-            //Console.Write("Введите число: ");
-            //double value = double.Parse(Console.ReadLine());
-
-            //Console.Write("Введите процент: ");
-            //double percent = double.Parse(Console.ReadLine());
-
-            //double result = (value * percent) / 100;
-            //Console.WriteLine(result);
-
-            //Task 3
-
-            //Console.WriteLine("Введите четыре цифры по одной:");
-
-            //Console.Write("Первая цифра: ");
-            //int num1 = int.Parse(Console.ReadLine());
-
-            //Console.Write("Вторая цифра: ");
-            //int num2 = int.Parse(Console.ReadLine());
-
-            //Console.Write("Третья цифра: ");
-            //int num3 = int.Parse(Console.ReadLine());
-
-            //Console.Write("Четвёртая цифра: ");
-            //int num4 = int.Parse(Console.ReadLine());
-
-
-            //int result = num1 * 1000 + num2 * 100 + num3 * 10 + num4;
-
-            //Console.WriteLine($"Сформированное число: {result}");
-
-            //Task 4
-
-            //Console.Write("Введите шестизначное число: ");
-            //int number = int.Parse(Console.ReadLine());
-
-            //if (number < 100000 || number > 999999)
-            //{
-            //    Console.WriteLine("Ошибка: необходимо ввести шестизначное число.");
-            //    return;
-            //}
-
-            //int firstDigit = number / 100000;
-            //int secondDigit = (number / 10000) % 10;
-            //int thirdDigit = (number / 1000) % 10;
-            //int fourthDigit = (number / 100) % 10;
-            //int fifthDigit = (number / 10) % 10;
-            //int sixthDigit = number % 10;
-
-            //int newNumber = sixthDigit * 100000 + secondDigit * 10000 + thirdDigit * 1000
-            //                + fourthDigit * 100 + fifthDigit * 10 + firstDigit;
-
-            //Console.WriteLine($"Результат после обмена: {newNumber}");
-
-            //Task 5
-
-            //Console.Write("Введите дату в формате дд.мм.гггг: ");
-            //string input = Console.ReadLine();
-
-            //int day = int.Parse(input.Substring(0, 2));
-            //int month = int.Parse(input.Substring(3, 2));
-            //int year = int.Parse(input.Substring(6, 4));
-
-
-            //string season = "";
-            //if (month == 12 && day >= 21 || month == 1 || month == 2 || month == 3 && day < 21)
-            //    season = "Winter";
-            //else if (month == 3 && day >= 21 || month == 4 || month == 5 || month == 6 && day < 21)
-            //    season = "Spring";
-            //else if (month == 6 && day >= 21 || month == 7 || month == 8 || month == 9 && day < 23)
-            //    season = "Summer";
-            //else if (month == 9 && day >= 23 || month == 10 || month == 11 || month == 12 && day < 21)
-            //    season = "Autumn";
-
-
-            //string dayOfWeek = "";
-            //int totalDays = (month - 1) * 30 + day; 
-            //totalDays += (year - 2000) * 365; 
-
-
-            //switch (totalDays % 7)
-            //{
-            //    case 0:
-            //        dayOfWeek = "Saturday";
-            //        break;
-            //    case 1:
-            //        dayOfWeek = "Sunday";
-            //        break;
-            //    case 2:
-            //        dayOfWeek = "Monday";
-            //        break;
-            //    case 3:
-            //        dayOfWeek = "Tuesday";
-            //        break;
-            //    case 4:
-            //        dayOfWeek = "Wednesday";
-            //        break;
-            //    case 5:
-            //        dayOfWeek = "Thursday";
-            //        break;
-            //    case 6:
-            //        dayOfWeek = "Friday";
-            //        break;
-            //}
-
-            //Console.WriteLine($"{season} {dayOfWeek}");
-
-            //Task 6
-
-            //Console.WriteLine("Выберите конвертацию:");
-            //Console.WriteLine("1. Фаренгейт в Цельсий");
-            //Console.WriteLine("2. Цельсий в Фаренгейт");
-            //Console.Write("Введите номер варианта (1 или 2): ");
-
-            //int choice = int.Parse(Console.ReadLine());
-            //double temperature, convertedTemperature;
-
-            //if (choice == 1)
-            //{
-            //    Console.Write("Введите температуру в Фаренгейтах: ");
-            //    temperature = double.Parse(Console.ReadLine());
-            //    convertedTemperature = (temperature - 32) * 5 / 9; 
-            //    Console.WriteLine($"Температура в Цельсии: {convertedTemperature}");
-            //}
-            //else if (choice == 2)
-            //{
-            //    Console.Write("Введите температуру в Цельсиях: ");
-            //    temperature = double.Parse(Console.ReadLine());
-            //    convertedTemperature = (temperature * 9 / 5) + 32;
-            //    Console.WriteLine($"Температура в Фаренгейтах: {convertedTemperature}");
-            //}
-            //else
-            //{
-            //    Console.WriteLine("Ошибка: неверный выбор.");
-            //}
-
-            //Task 7
-
-            Console.Write("Введите первое число: ");
-            int start = int.Parse(Console.ReadLine());
-
-            Console.Write("Введите второе число: ");
-            int end = int.Parse(Console.ReadLine());
-
             
-            if (start > end)
-            {
-                int temp = start;
-                start = end;
-                end = temp;
-            }
-
-            Console.WriteLine($"Чётные числа в диапазоне от {start} до {end}:");
-            for (int i = start; i <= end; i++)
-            {
-                if (i % 2 == 0)
-                {
-                    Console.WriteLine(i);
-                }
-            }
         }
     }
 }
